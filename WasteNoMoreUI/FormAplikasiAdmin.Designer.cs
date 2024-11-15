c7f7a62d1041b805280aea34a627d9cfb5ba6dfc
Nur Rochman <nurrochman954@gmail.com>
2024-11-16 00:34:13 +0700

    Update FormAplikasiAdmin

--- WasteNoMoreUI/FormAplikasiAdmin.Designer.cs
+++ WasteNoMoreUI/FormAplikasiAdmin.Designer.cs
@@ -28,12 +28,273 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormAplikasiAdmin";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAplikasiAdmin));
+            btnBackAplikasi = new Button();
+            lblAplikasi = new Label();
+            dgvAplikasi = new DataGridView();
+            lblUpdate = new Label();
+            lblDelete = new Label();
+            lblInsert = new Label();
+            pbDeleteAplikasi = new PictureBox();
+            pbUpdateAplikasi = new PictureBox();
+            pbInsertAplikasi = new PictureBox();
+            btnLoadAplikasi = new Button();
+            txtNamaAplikasi = new TextBox();
+            txtDeskripsiAplikasi = new TextBox();
+            lblEmail = new Label();
+            lblNamaPengguna = new Label();
+            cbKategori = new ComboBox();
+            lblKategoriAplikasi = new Label();
+            txtHarga = new TextBox();
+            lblHarga = new Label();
+            ((System.ComponentModel.ISupportInitialize)dgvAplikasi).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbDeleteAplikasi).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbUpdateAplikasi).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbInsertAplikasi).BeginInit();
+            SuspendLayout();
+            // 
+            // btnBackAplikasi
+            // 
+            btnBackAplikasi.BackColor = SystemColors.ControlLight;
+            btnBackAplikasi.Location = new Point(1028, 654);
+            btnBackAplikasi.Margin = new Padding(2);
+            btnBackAplikasi.Name = "btnBackAplikasi";
+            btnBackAplikasi.Size = new Size(115, 36);
+            btnBackAplikasi.TabIndex = 98;
+            btnBackAplikasi.Text = "Back";
+            btnBackAplikasi.UseVisualStyleBackColor = false;
+            btnBackAplikasi.Click += btnBackAplikasi_Click;
+            // 
+            // lblAplikasi
+            // 
+            lblAplikasi.AutoSize = true;
+            lblAplikasi.Font = new Font("Segoe UI Black", 13.875F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblAplikasi.Location = new Point(485, 25);
+            lblAplikasi.Margin = new Padding(2, 0, 2, 0);
+            lblAplikasi.Name = "lblAplikasi";
+            lblAplikasi.Size = new Size(331, 38);
+            lblAplikasi.TabIndex = 97;
+            lblAplikasi.Text = "APLIKASI DAN HARGA";
+            // 
+            // dgvAplikasi
+            // 
+            dgvAplikasi.BackgroundColor = SystemColors.ButtonHighlight;
+            dgvAplikasi.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvAplikasi.Location = new Point(86, 348);
+            dgvAplikasi.Margin = new Padding(2);
+            dgvAplikasi.Name = "dgvAplikasi";
+            dgvAplikasi.RowHeadersWidth = 82;
+            dgvAplikasi.Size = new Size(1057, 221);
+            dgvAplikasi.TabIndex = 96;
+            // 
+            // lblUpdate
+            // 
+            lblUpdate.AutoSize = true;
+            lblUpdate.Font = new Font("Segoe UI Semibold", 10.875F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblUpdate.Location = new Point(858, 237);
+            lblUpdate.Margin = new Padding(2, 0, 2, 0);
+            lblUpdate.Name = "lblUpdate";
+            lblUpdate.Size = new Size(85, 30);
+            lblUpdate.TabIndex = 95;
+            lblUpdate.Text = "Update";
+            // 
+            // lblDelete
+            // 
+            lblDelete.AutoSize = true;
+            lblDelete.Font = new Font("Segoe UI Semibold", 10.875F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblDelete.Location = new Point(1053, 237);
+            lblDelete.Margin = new Padding(2, 0, 2, 0);
+            lblDelete.Name = "lblDelete";
+            lblDelete.Size = new Size(79, 30);
+            lblDelete.TabIndex = 94;
+            lblDelete.Text = "Delete";
+            // 
+            // lblInsert
+            // 
+            lblInsert.AutoSize = true;
+            lblInsert.Font = new Font("Segoe UI Semibold", 10.875F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblInsert.Location = new Point(666, 234);
+            lblInsert.Margin = new Padding(2, 0, 2, 0);
+            lblInsert.Name = "lblInsert";
+            lblInsert.Size = new Size(70, 30);
+            lblInsert.TabIndex = 93;
+            lblInsert.Text = "Insert";
+            // 
+            // pbDeleteAplikasi
+            // 
+            pbDeleteAplikasi.Image = (Image)resources.GetObject("pbDeleteAplikasi.Image");
+            pbDeleteAplikasi.Location = new Point(1028, 122);
+            pbDeleteAplikasi.Margin = new Padding(2);
+            pbDeleteAplikasi.Name = "pbDeleteAplikasi";
+            pbDeleteAplikasi.Size = new Size(115, 112);
+            pbDeleteAplikasi.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbDeleteAplikasi.TabIndex = 92;
+            pbDeleteAplikasi.TabStop = false;
+            pbDeleteAplikasi.Click += pbDeleteAplikasi_Click;
+            // 
+            // pbUpdateAplikasi
+            // 
+            pbUpdateAplikasi.Image = (Image)resources.GetObject("pbUpdateAplikasi.Image");
+            pbUpdateAplikasi.Location = new Point(841, 122);
+            pbUpdateAplikasi.Margin = new Padding(2);
+            pbUpdateAplikasi.Name = "pbUpdateAplikasi";
+            pbUpdateAplikasi.Size = new Size(115, 112);
+            pbUpdateAplikasi.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbUpdateAplikasi.TabIndex = 91;
+            pbUpdateAplikasi.TabStop = false;
+            pbUpdateAplikasi.Click += pbUpdateAplikasi_Click;
+            // 
+            // pbInsertAplikasi
+            // 
+            pbInsertAplikasi.Image = (Image)resources.GetObject("pbInsertAplikasi.Image");
+            pbInsertAplikasi.Location = new Point(639, 114);
+            pbInsertAplikasi.Margin = new Padding(2);
+            pbInsertAplikasi.Name = "pbInsertAplikasi";
+            pbInsertAplikasi.Size = new Size(126, 130);
+            pbInsertAplikasi.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbInsertAplikasi.TabIndex = 90;
+            pbInsertAplikasi.TabStop = false;
+            pbInsertAplikasi.Click += pbInsertAplikasi_Click;
+            // 
+            // btnLoadAplikasi
+            // 
+            btnLoadAplikasi.BackColor = SystemColors.Highlight;
+            btnLoadAplikasi.ForeColor = SystemColors.ButtonHighlight;
+            btnLoadAplikasi.Location = new Point(589, 591);
+            btnLoadAplikasi.Margin = new Padding(2);
+            btnLoadAplikasi.Name = "btnLoadAplikasi";
+            btnLoadAplikasi.Size = new Size(115, 36);
+            btnLoadAplikasi.TabIndex = 89;
+            btnLoadAplikasi.Text = "Load";
+            btnLoadAplikasi.UseVisualStyleBackColor = false;
+            // 
+            // txtNamaAplikasi
+            // 
+            txtNamaAplikasi.Location = new Point(199, 108);
+            txtNamaAplikasi.Margin = new Padding(2);
+            txtNamaAplikasi.Name = "txtNamaAplikasi";
+            txtNamaAplikasi.Size = new Size(326, 31);
+            txtNamaAplikasi.TabIndex = 88;
+            // 
+            // txtDeskripsiAplikasi
+            // 
+            txtDeskripsiAplikasi.Location = new Point(199, 205);
+            txtDeskripsiAplikasi.Margin = new Padding(2);
+            txtDeskripsiAplikasi.Multiline = true;
+            txtDeskripsiAplikasi.Name = "txtDeskripsiAplikasi";
+            txtDeskripsiAplikasi.Size = new Size(326, 85);
+            txtDeskripsiAplikasi.TabIndex = 87;
+            // 
+            // lblEmail
+            // 
+            lblEmail.AutoSize = true;
+            lblEmail.Location = new Point(91, 209);
+            lblEmail.Margin = new Padding(2, 0, 2, 0);
+            lblEmail.Name = "lblEmail";
+            lblEmail.Size = new Size(93, 25);
+            lblEmail.TabIndex = 86;
+            lblEmail.Text = "Deskripsi :";
+            // 
+            // lblNamaPengguna
+            // 
+            lblNamaPengguna.AutoSize = true;
+            lblNamaPengguna.Location = new Point(116, 111);
+            lblNamaPengguna.Margin = new Padding(2, 0, 2, 0);
+            lblNamaPengguna.Name = "lblNamaPengguna";
+            lblNamaPengguna.Size = new Size(68, 25);
+            lblNamaPengguna.TabIndex = 85;
+            lblNamaPengguna.Text = "Nama :";
+            // 
+            // cbKategori
+            // 
+            cbKategori.FormattingEnabled = true;
+            cbKategori.Location = new Point(199, 153);
+            cbKategori.Name = "cbKategori";
+            cbKategori.Size = new Size(326, 33);
+            cbKategori.TabIndex = 99;
+            cbKategori.SelectedIndexChanged += cbKategori_SelectedIndexChanged;
+            // 
+            // lblKategoriAplikasi
+            // 
+            lblKategoriAplikasi.AutoSize = true;
+            lblKategoriAplikasi.Location = new Point(97, 156);
+            lblKategoriAplikasi.Margin = new Padding(2, 0, 2, 0);
+            lblKategoriAplikasi.Name = "lblKategoriAplikasi";
+            lblKategoriAplikasi.Size = new Size(87, 25);
+            lblKategoriAplikasi.TabIndex = 100;
+            lblKategoriAplikasi.Text = "Kategori :";
+            // 
+            // txtHarga
+            // 
+            txtHarga.Location = new Point(199, 303);
+            txtHarga.Margin = new Padding(2);
+            txtHarga.Name = "txtHarga";
+            txtHarga.Size = new Size(326, 31);
+            txtHarga.TabIndex = 101;
+            // 
+            // lblHarga
+            // 
+            lblHarga.AutoSize = true;
+            lblHarga.Location = new Point(54, 303);
+            lblHarga.Margin = new Padding(2, 0, 2, 0);
+            lblHarga.Name = "lblHarga";
+            lblHarga.Size = new Size(130, 25);
+            lblHarga.TabIndex = 102;
+            lblHarga.Text = "Harga (per kg):";
+            // 
+            // FormAplikasiAdmin
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1229, 712);
+            Controls.Add(lblHarga);
+            Controls.Add(txtHarga);
+            Controls.Add(lblKategoriAplikasi);
+            Controls.Add(cbKategori);
+            Controls.Add(btnBackAplikasi);
+            Controls.Add(lblAplikasi);
+            Controls.Add(dgvAplikasi);
+            Controls.Add(lblUpdate);
+            Controls.Add(lblDelete);
+            Controls.Add(lblInsert);
+            Controls.Add(pbDeleteAplikasi);
+            Controls.Add(pbUpdateAplikasi);
+            Controls.Add(pbInsertAplikasi);
+            Controls.Add(btnLoadAplikasi);
+            Controls.Add(txtNamaAplikasi);
+            Controls.Add(txtDeskripsiAplikasi);
+            Controls.Add(lblEmail);
+            Controls.Add(lblNamaPengguna);
+            Name = "FormAplikasiAdmin";
+            Text = "FormAplikasiAdmin";
+            Load += FormAplikasiAdmin_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvAplikasi).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbDeleteAplikasi).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbUpdateAplikasi).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbInsertAplikasi).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnBackAplikasi;
+        private Label lblAplikasi;
+        private DataGridView dgvAplikasi;
+        private Label lblUpdate;
+        private Label lblDelete;
+        private Label lblInsert;
+        private PictureBox pbDeleteAplikasi;
+        private PictureBox pbUpdateAplikasi;
+        private PictureBox pbInsertAplikasi;
+        private Button btnLoadAplikasi;
+        private TextBox txtNamaAplikasi;
+        private TextBox txtDeskripsiAplikasi;
+        private Label lblEmail;
+        private Label lblNamaPengguna;
+        private ComboBox cbKategori;
+        private Label lblKategoriAplikasi;
+        private TextBox txtHarga;
+        private Label lblHarga;
     }
 }
--- WasteNoMoreUI/FormAplikasiAdmin.cs
+++ WasteNoMoreUI/FormAplikasiAdmin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,343 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WasteNoMoreUI.Models;
 
 namespace WasteNoMoreUI
 {
     public partial class FormAplikasiAdmin : Form
     {
+        private DataTable dt;
+        private DataGridViewRow r;
         public FormAplikasiAdmin()
         {
             InitializeComponent();
+        }
+
+        private void LoadKategoriToComboBox()
+        {
+            try
+            {
+                // Bersihkan item di combobox terlebih dahulu
+                cbKategori.Items.Clear();
+
+                using (var conn = DatabaseManager.GetConnection())
+                {
+                    conn.Open();
+                    using (var cmd = new NpgsqlCommand("SELECT nama_kategori FROM kategori WHERE is_deleted = FALSE ORDER BY nama_kategori ASC;", conn))
+                    {
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                // Tambahkan nama kategori ke ComboBox
+                                cbKategori.Items.Add(reader["nama_kategori"].ToString());
+                            }
+                        }
+                    }
+                }
+
+                // Beri placeholder jika tidak ada kategori
+                if (cbKategori.Items.Count == 0)
+                {
+                    cbKategori.Items.Add("-- Tidak Ada Kategori --");
+                }
+
+                cbKategori.SelectedIndex = 0; // Pilih item pertama secara default
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Load Kategori FAIL!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        private void pbInsertAplikasi_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtNamaAplikasi.Text) || cbKategori.SelectedIndex == -1 || string.IsNullOrWhiteSpace(txtHarga.Text))
+                {
+                    MessageBox.Show("Pastikan semua field telah diisi!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                using (var conn = DatabaseManager.GetConnection())
+                {
+                    conn.Open();
+
+                    // Mulai transaksi untuk memastikan konsistensi data
+                    using (var trans = conn.BeginTransaction())
+                    {
+                        try
+                        {
+                            // Dapatkan ID kategori berdasarkan nama yang dipilih di ComboBox
+                            int idKategori;
+                            using (var cmdKategori = new NpgsqlCommand("SELECT id_kategori FROM kategori WHERE nama_kategori = @nama_kategori AND is_deleted = FALSE", conn, trans))
+                            {
+                                cmdKategori.Parameters.AddWithValue("nama_kategori", cbKategori.SelectedItem.ToString());
+                                var result = cmdKategori.ExecuteScalar();
+                                if (result == null)
+                                {
+                                    throw new Exception("Kategori tidak ditemukan. Pastikan kategori valid.");
+                                }
+                                idKategori = Convert.ToInt32(result);
+                            }
+
+                            // Masukkan data ke tabel aplikasi
+                            int idAplikasi;
+                            using (var cmdAplikasi = new NpgsqlCommand(
+                                "INSERT INTO aplikasi (nama_aplikasi, deskripsi_aplikasi) VALUES (@nama_aplikasi, @deskripsi_aplikasi) RETURNING id_aplikasi",
+                                conn, trans))
+                            {
+                                cmdAplikasi.Parameters.AddWithValue("nama_aplikasi", txtNamaAplikasi.Text.Trim());
+                                cmdAplikasi.Parameters.AddWithValue("deskripsi_aplikasi", string.IsNullOrWhiteSpace(txtDeskripsiAplikasi.Text) ? (object)DBNull.Value : txtDeskripsiAplikasi.Text.Trim());
+
+                                idAplikasi = Convert.ToInt32(cmdAplikasi.ExecuteScalar());
+                            }
+
+                            // Masukkan data ke tabel harga
+                            using (var cmdHarga = new NpgsqlCommand(
+                                "INSERT INTO harga (id_kategori, id_aplikasi, harga) VALUES (@id_kategori, @id_aplikasi, @harga)",
+                                conn, trans))
+                            {
+                                cmdHarga.Parameters.AddWithValue("id_kategori", idKategori);
+                                cmdHarga.Parameters.AddWithValue("id_aplikasi", idAplikasi);
+                                cmdHarga.Parameters.AddWithValue("harga", Convert.ToDecimal(txtHarga.Text.Trim()));
+
+                                cmdHarga.ExecuteNonQuery();
+                            }
+
+                            // Commit transaksi
+                            trans.Commit();
+
+                            MessageBox.Show("Aplikasi berhasil ditambahkan!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                            // Bersihkan input dan refresh data grid
+                            txtNamaAplikasi.Clear();
+                            txtDeskripsiAplikasi.Clear();
+                            txtHarga.Clear();
+                            cbKategori.SelectedIndex = 0;
+                            LoadData();
+                        }
+                        catch
+                        {
+                            // Rollback jika ada error
+                            trans.Rollback();
+                            throw;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Insert FAIL!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        private void LoadData()
+        {
+            try
+            {
+                // Query untuk memanggil function di database
+                string query = "SELECT * FROM get_aplikasi_data()";
+
+                // Eksekusi query dan simpan hasil ke DataTable
+                dt = DatabaseManager.ExecuteQuery(query);
+
+                // Tampilkan data di DataGridView
+                dgvAplikasi.DataSource = dt;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Load FAIL!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+
+        private void FormAplikasiAdmin_Load(object sender, EventArgs e)
+        {
+            // Muat data ke DataGridView
+            LoadData();
+
+            // Muat data kategori ke ComboBox
+            LoadKategoriToComboBox();
+        }
+
+        private void cbKategori_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Ambil nama kategori yang dipilih dari ComboBox
+            string selectedKategori = cbKategori.SelectedItem.ToString();
+        }
+
+        private void pbUpdateAplikasi_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dgvAplikasi.SelectedRows.Count == 0)
+                {
+                    MessageBox.Show("Pilih aplikasi yang ingin diperbarui!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                var selectedRow = dgvAplikasi.SelectedRows[0];
+                int idAplikasi = Convert.ToInt32(selectedRow.Cells["id_aplikasi"].Value);
+
+                if (string.IsNullOrWhiteSpace(txtNamaAplikasi.Text) || cbKategori.SelectedIndex == -1 || string.IsNullOrWhiteSpace(txtHarga.Text))
+                {
+                    MessageBox.Show("Pastikan semua field telah diisi!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                using (var conn = DatabaseManager.GetConnection())
+                {
+                    conn.Open();
+
+                    // Mulai transaksi untuk memastikan konsistensi data
+                    using (var trans = conn.BeginTransaction())
+                    {
+                        try
+                        {
+                            // Dapatkan ID kategori berdasarkan nama yang dipilih di ComboBox
+                            int idKategori;
+                            using (var cmdKategori = new NpgsqlCommand("SELECT id_kategori FROM kategori WHERE nama_kategori = @nama_kategori AND is_deleted = FALSE", conn, trans))
+                            {
+                                cmdKategori.Parameters.AddWithValue("nama_kategori", cbKategori.SelectedItem.ToString());
+                                var result = cmdKategori.ExecuteScalar();
+                                if (result == null)
+                                {
+                                    throw new Exception("Kategori tidak ditemukan. Pastikan kategori valid.");
+                                }
+                                idKategori = Convert.ToInt32(result);
+                            }
+
+                            // Update data aplikasi
+                            using (var cmdAplikasi = new NpgsqlCommand(
+                                "UPDATE aplikasi SET nama_aplikasi = @nama_aplikasi, deskripsi_aplikasi = @deskripsi_aplikasi WHERE id_aplikasi = @id_aplikasi",
+                                conn, trans))
+                            {
+                                cmdAplikasi.Parameters.AddWithValue("id_aplikasi", idAplikasi);
+                                cmdAplikasi.Parameters.AddWithValue("nama_aplikasi", txtNamaAplikasi.Text.Trim());
+                                cmdAplikasi.Parameters.AddWithValue("deskripsi_aplikasi", string.IsNullOrWhiteSpace(txtDeskripsiAplikasi.Text) ? (object)DBNull.Value : txtDeskripsiAplikasi.Text.Trim());
+
+                                cmdAplikasi.ExecuteNonQuery();
+                            }
+
+                            // Update harga
+                            using (var cmdHarga = new NpgsqlCommand(
+                                "UPDATE harga SET id_kategori = @id_kategori, harga = @harga WHERE id_aplikasi = @id_aplikasi",
+                                conn, trans))
+                            {
+                                cmdHarga.Parameters.AddWithValue("id_kategori", idKategori);
+                                cmdHarga.Parameters.AddWithValue("id_aplikasi", idAplikasi);
+                                cmdHarga.Parameters.AddWithValue("harga", Convert.ToDecimal(txtHarga.Text.Trim()));
+
+                                cmdHarga.ExecuteNonQuery();
+                            }
+
+                            // Commit transaksi
+                            trans.Commit();
+
+                            MessageBox.Show("Aplikasi berhasil diperbarui!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                            // Bersihkan input dan refresh data grid
+                            txtNamaAplikasi.Clear();
+                            txtDeskripsiAplikasi.Clear();
+                            txtHarga.Clear();
+                            cbKategori.SelectedIndex = 0;
+                            LoadData();
+                        }
+                        catch
+                        {
+                            // Rollback jika ada error
+                            trans.Rollback();
+                            throw;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Update FAIL!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void pbDeleteAplikasi_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dgvAplikasi.SelectedRows.Count == 0)
+                {
+                    MessageBox.Show("Pilih aplikasi yang ingin dihapus!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                var selectedRow = dgvAplikasi.SelectedRows[0];
+                int idAplikasi = Convert.ToInt32(selectedRow.Cells["id_aplikasi"].Value);
+
+                DialogResult result = MessageBox.Show("Apakah Anda yakin ingin menghapus aplikasi ini?", "Konfirmasi Hapus", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.No)
+                {
+                    return;
+                }
+
+                using (var conn = DatabaseManager.GetConnection())
+                {
+                    conn.Open();
+
+                    // Mulai transaksi untuk memastikan konsistensi data
+                    using (var trans = conn.BeginTransaction())
+                    {
+                        try
+                        {
+                            // Hapus data dari tabel harga
+                            using (var cmdHarga = new NpgsqlCommand("DELETE FROM harga WHERE id_aplikasi = @id_aplikasi", conn, trans))
+                            {
+                                cmdHarga.Parameters.AddWithValue("id_aplikasi", idAplikasi);
+                                cmdHarga.ExecuteNonQuery();
+                            }
+
+                            // Hapus data dari tabel aplikasi
+                            using (var cmdAplikasi = new NpgsqlCommand("DELETE FROM aplikasi WHERE id_aplikasi = @id_aplikasi", conn, trans))
+                            {
+                                cmdAplikasi.Parameters.AddWithValue("id_aplikasi", idAplikasi);
+                                cmdAplikasi.ExecuteNonQuery();
+                            }
+
+                            // Commit transaksi
+                            trans.Commit();
+
+                            MessageBox.Show("Aplikasi berhasil dihapus!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                            // Refresh data grid
+                            LoadData();
+                        }
+                        catch
+                        {
+                            // Rollback jika ada error
+                            trans.Rollback();
+                            throw;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Delete FAIL!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnBackAplikasi_Click(object sender, EventArgs e)
+        {
+            FormDashobardAdmin dashobardAdminForm = new FormDashobardAdmin();
+            dashobardAdminForm.Show();
+            this.Hide();
         }
     }
 }
